e0bf876bbd9b980b209a2dadb2931eb4955bbec0
Seong <sryoo6@gatech.edu>
2020-08-04 12:33:07 -0400

    Show current post tag data

--- assets/js/opp.js
+++ assets/js/opp.js
@@ -5,6 +5,8 @@
   // database api
   const withSelect = wp.data.withSelect;
   const withDispatch = wp.data.withDispatch;
+  // Components
+  const CheckboxControl = wp.components.CheckboxControl;
   const fetchTerms = withSelect(function(select) {
     const queryArgs = {
       per_page: -1,
@@ -24,23 +26,65 @@
         'taxon_type',
         queryArgs
     );
+    const typeData = select('core/editor').getCurrentPostAttribute('taxon_type');
+    const timeData = select('core/editor').getCurrentPostAttribute('taxon_time');
+    const locData  = select('core/editor').getCurrentPostAttribute('taxon_loc');
     return {
       times: timeTerms,
       types: typeTerms,
       locs: locTerms,
+      chosenTypes: typeData,
+      chosenTimes: timeData,
+      chosenLocs: locData,
     };
   });
   const oppEdit = fetchTerms(function(props) {
     if (!props.times || !props.types || !props.locs) {
       return 'Fetching tags...';
     }
+    if (!props.chosenTimes || !props.chosenTypes || !props.chosenLocs) {
+      return 'Fetching post data...';
+    }
     console.log(props.times);
     console.log(props.types);
     console.log(props.locs);
+    const generateTags = function(data, arr, chosenTags) {
+      const isChecked = function(id) {
+        for (const tagId of chosenTags) {
+          if (id == tagId) {
+            return true;
+          }
+        }
+        return false;
+      }
+      for (const tag of data) {
+        const id = tag.id;
+        const name = tag.name;
+        const checkbox = el(
+            CheckboxControl,
+            {
+              'className': 'appia-tag',
+              'data-id': id,
+              'checked': isChecked(id),
+              'label': name,
+              'onChange': function(value) {
+                console.log(value)
+              }
+            }
+        );
+        arr.push(checkbox);
+      }
+    }
+    const timeTags = [];
+    generateTags(props.times, timeTags, props.chosenTimes);
+    const typeTags = [];
+    generateTags(props.types, typeTags, props.chosenTypes);
+    const locTags = [];
+    generateTags(props.locs, locTags, props.chosenLocs);
     return el(
         'div',
         {},
-        'What?'
+        [timeTags, typeTags, locTags]
     );
   });
   const oppArgs = {
